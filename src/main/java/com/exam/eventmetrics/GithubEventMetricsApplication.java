bc4a63264c4d87dbdc44aac5bc9db776a199c52c
300069965 <abhishyam.chennayipalem@myntra.com>
2020-07-16 21:39:49 +0530

    first commit

---
+++ src/main/java/com/exam/eventmetrics/GithubEventMetricsApplication.java
@@ -0,0 +1,13 @@
+package com.exam.eventmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubEventMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubEventMetricsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exam/eventmetrics/GithubEventMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exam.eventmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubEventMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
